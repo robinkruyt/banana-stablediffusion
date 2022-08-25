a70792264324f9b21ca255de5babca80dc8b4781
Robin Kruijt <robin@robinkruijt.com>
2022-08-25 12:17:05 +0200

    Setup for stable diffusion

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,39 @@
 # Must use a Cuda version 11+
 FROM pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime
 
-WORKDIR /
+WORKDIR /app
 
 # Install git
-RUN apt-get update && apt-get install -y git
+RUN apt-get update && apt-get install -y git curl wget && apt-get clean && rm -rf /var/lib/apt/lists/*
 
+# Setup conda
+# ENV CONDA_DIR /opt/conda
+# RUN wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh && \
+#      /bin/bash ~/miniconda.sh -b -p /opt/conda
+
+# # Put conda in path so we can use conda activate
+# ENV PATH=$CONDA_DIR/bin:$PATH
+
+
+# Install stable diffusion
+RUN git clone https://github.com/CompVis/stable-diffusion.git stablediffusion
+
+# Install checkpoint
+RUN curl https://www.googleapis.com/storage/v1/b/aai-blog-files/o/sd-v1-4.ckpt?alt=media > /app/stablediffusion/sd-v1-4.ckpt
+
+# Create env for stable diffusion
+RUN cd /app/stablediffusion && conda env create -f environment.yaml
+
+# Set a new shell to include conda env
+SHELL ["conda", "run", "-n", "ldm", "/bin/bash", "-c"]
+
+
+# App specific code
 # Install python packages
-RUN pip3 install --upgrade pip
-ADD requirements.txt requirements.txt
-RUN pip3 install -r requirements.txt
+RUN conda install sanic transformers accelerate
 
 # We add the banana boilerplate here
 ADD server.py .
-
-# Add your model weight files 
-# (in this case we have a python script)
-ADD download.py .
-RUN python3 download.py
-
 
 # Add your custom app code, init() and inference()
 ADD app.py .
--- app.py
+++ app.py
@@ -1,26 +1,236 @@
-from transformers import pipeline
+import argparse, os, sys, glob
+import cv2
 import torch
+import numpy as np
+from omegaconf import OmegaConf
+from PIL import Image
+from tqdm import tqdm, trange
+from imwatermark import WatermarkEncoder
+from itertools import islice
+from einops import rearrange
+from torchvision.utils import make_grid
+import time
+from pytorch_lightning import seed_everything
+from torch import autocast
+from contextlib import contextmanager, nullcontext
+
+from stablediffusion.ldm.util import instantiate_from_config
+from stablediffusion.ldm.models.diffusion.ddim import DDIMSampler
+from stablediffusion.ldm.models.diffusion.plms import PLMSSampler
+
+from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
+from transformers import AutoFeatureExtractor, pipeline
+
+import base64
+from io import BytesIO
+
+# Stable diffusion is available in /app/stablediffusion
+
+model = None
+
+# load safety model
+safety_model_id = "CompVis/stable-diffusion-safety-checker"
+safety_feature_extractor = AutoFeatureExtractor.from_pretrained(safety_model_id)
+safety_checker = StableDiffusionSafetyChecker.from_pretrained(safety_model_id)
+
+
+def chunk(it, size):
+    it = iter(it)
+    return iter(lambda: tuple(islice(it, size)), ())
+
+
+def numpy_to_pil(images):
+    """
+    Convert a numpy image or a batch of images to a PIL image.
+    """
+    if images.ndim == 3:
+        images = images[None, ...]
+    images = (images * 255).round().astype("uint8")
+    pil_images = [Image.fromarray(image) for image in images]
+
+    return pil_images
+
+
+def load_model_from_config(config, ckpt, verbose=False):
+    print(f"Loading model from {ckpt}")
+    pl_sd = torch.load(ckpt, map_location="cpu")
+    if "global_step" in pl_sd:
+        print(f"Global Step: {pl_sd['global_step']}")
+    sd = pl_sd["state_dict"]
+    model = instantiate_from_config(config.model)
+    m, u = model.load_state_dict(sd, strict=False)
+    if len(m) > 0 and verbose:
+        print("missing keys:")
+        print(m)
+    if len(u) > 0 and verbose:
+        print("unexpected keys:")
+        print(u)
+
+    model.cuda()
+    model.eval()
+    return model
+
+
+def put_watermark(img, wm_encoder=None):
+    if wm_encoder is not None:
+        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+        img = wm_encoder.encode(img, 'dwtDct')
+        img = Image.fromarray(img[:, :, ::-1])
+    return img
+
+
+def load_replacement(x):
+    try:
+        hwc = x.shape
+        y = Image.open("stablediffusion/assets/rick.jpeg").convert("RGB").resize((hwc[1], hwc[0]))
+        y = (np.array(y)/255.0).astype(x.dtype)
+        assert y.shape == x.shape
+        return y
+    except Exception:
+        return x
+
+
+def check_safety(x_image):
+    safety_checker_input = safety_feature_extractor(numpy_to_pil(x_image), return_tensors="pt")
+    x_checked_image, has_nsfw_concept = safety_checker(images=x_image, clip_input=safety_checker_input.pixel_values)
+    assert x_checked_image.shape[0] == len(has_nsfw_concept)
+    for i in range(len(has_nsfw_concept)):
+        if has_nsfw_concept[i]:
+            x_checked_image[i] = load_replacement(x_checked_image[i])
+    return x_checked_image, has_nsfw_concept
+
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
-    
-    device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
 
+    seed_everything(42)
+
+    config = OmegaConf.load(f"/app/stablediffusion/configs/stable-diffusion/v1-inference.yaml")
+    model = load_model_from_config(config, f"/app/stablediffusion/sd-v1-4.ckpt")
+
+    device = 0 if torch.cuda.is_available() else -1
+    model = model.to(device)
+    
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
     global model
 
-    # Parse out your arguments
-    prompt = model_inputs.get('prompt', None)
-    if prompt == None:
+    device = 0 if torch.cuda.is_available() else -1
+
+    image_result = None
+
+    prompt = model_inputs.get('prompt', "a flying dog")
+    if prompt is None:
         return {'message': "No prompt provided"}
     
-    # Run the model
-    result = model(prompt)
+    outdir = model_inputs.get('outdir', "/tmp/sd")
+
+    skip_grid = model_inputs.get('skip_grid', False)
+    ddim_steps = model_inputs.get('ddim_steps', 50)
+    fixed_code = model_inputs.get('fixed_code', False)
+    ddim_eta = model_inputs.get('ddim_eta', 0.0)
+    n_iter = model_inputs.get('n_iter', 2)
+    height = model_inputs.get('height', 512)
+    width = model_inputs.get('width', 512)
+    C = model_inputs.get('C', 4) # Latent channels
+    f = model_inputs.get('f', 8) # Downsampling factor
+    n_samples = model_inputs.get('n_samples', 3)
+    n_rows = model_inputs.get('n_rows', 0)
+    scale = model_inputs.get('scale', 7.5)
+    precision = model_inputs.get('precision', "autocast")
+
+    sampler = DDIMSampler(model)
+
+    os.makedirs(outdir, exist_ok=True)
+    outpath = outdir
+
+    print("Creating invisible watermark encoder (see https://github.com/ShieldMnt/invisible-watermark)...")
+    wm = "StableDiffusionV1"
+    wm_encoder = WatermarkEncoder()
+    wm_encoder.set_watermark('bytes', wm.encode('utf-8'))
+
+    batch_size = n_samples
+    n_rows = n_rows if n_rows > 0 else batch_size
+
+
+    data = [batch_size * [prompt]]
+
+    sample_path = os.path.join(outpath, "samples")
+    os.makedirs(sample_path, exist_ok=True)
+    base_count = len(os.listdir(sample_path))
+    grid_count = len(os.listdir(outpath)) - 1
+
+    start_code = None
+    if fixed_code:
+        start_code = torch.randn([n_samples, C, height // f, width // f], device=device)
+
+    precision_scope = autocast if precision=="autocast" else nullcontext
+    with torch.no_grad():
+        with precision_scope("cuda"):
+            with model.ema_scope():
+                tic = time.time()
+                all_samples = list()
+                for n in trange(n_iter, desc="Sampling"):
+                    for prompts in tqdm(data, desc="data"):
+                        uc = None
+                        if scale != 1.0:
+                            uc = model.get_learned_conditioning(batch_size * [""])
+                        if isinstance(prompts, tuple):
+                            prompts = list(prompts)
+                        c = model.get_learned_conditioning(prompts)
+                        shape = [C, height // f, width // f]
+                        samples_ddim, _ = sampler.sample(S=ddim_steps,
+                                                         conditioning=c,
+                                                         batch_size=n_samples,
+                                                         shape=shape,
+                                                         verbose=False,
+                                                         unconditional_guidance_scale=scale,
+                                                         unconditional_conditioning=uc,
+                                                         eta=ddim_eta,
+                                                         x_T=start_code)
+
+                        x_samples_ddim = model.decode_first_stage(samples_ddim)
+                        x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
+                        x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
+
+                        x_checked_image, has_nsfw_concept = check_safety(x_samples_ddim)
+
+                        x_checked_image_torch = torch.from_numpy(x_checked_image).permute(0, 3, 1, 2)
+
+                        for x_sample in x_checked_image_torch:
+                            x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
+                            img = Image.fromarray(x_sample.astype(np.uint8))
+                            img = put_watermark(img, wm_encoder)
+                            img.save(os.path.join(sample_path, f"{base_count:05}.png"))
+                            base_count += 1
+
+                        if not skip_grid:
+                            all_samples.append(x_checked_image_torch)
+
+                if not skip_grid:
+                    # additionally, save as grid
+                    grid = torch.stack(all_samples, 0)
+                    grid = rearrange(grid, 'n b c h w -> (n b) c h w')
+                    grid = make_grid(grid, nrow=n_rows)
+
+                    # to image
+                    grid = 255. * rearrange(grid, 'c h w -> h w c').cpu().numpy()
+                    grid_img = Image.fromarray(grid.astype(np.uint8))
+                    grid_img = put_watermark(img, wm_encoder)
+                    grid_img.save(os.path.join(outpath, f'grid-{grid_count:04}.png'))
+                    grid_count += 1
+
+                toc = time.time()
+    
+    # return results
+    buffered = BytesIO()
+    grid_img.save(buffered, format="JPEG")
+    image_result = base64.b64encode(buffered.getvalue())
+
+    result = {"tic": tic, "toc": toc, "duration": toc-tic, "grid": image_result}
 
     # Return the results as a dictionary
     return result
